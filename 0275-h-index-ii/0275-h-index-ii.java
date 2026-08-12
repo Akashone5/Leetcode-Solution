7de5f716cb0bf2b37c349d981c8a5f7160c3db78
Akash <akashone15@gmail.com>
2026-08-12 22:00:09 +0530

    Time: 1 ms (22.69%), Space: 50.1 MB (66.21%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int l=citations.length;
+        for(int i=0;i<l;i++){
+            if(citations[i]>=(l-i)){
+                return l-i;
+            }
+        }
+        return 0;
+    }
+}
